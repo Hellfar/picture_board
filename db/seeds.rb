b9610e7a5dd4deb21856f797b9c49a0e4e6fd42f
pierre.l <pierre.l@snapkin.fr>
2017-02-20 20:31:06 +0100

    Seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+provider = User.create email: "provider@example.org", password: "password"
+customer = User.create email: "customer@example.org", password: "password"
+
+Picture.create user: provider, path: "http://cdn3-www.comingsoon.net/assets/uploads/gallery/doctor-strange-1403135280/screen-shot-2016-04-06-at-10-35-22-am.png"
+Picture.create user: provider, path: "http://static2.comicvine.com/uploads/scale_small/11/113509/4426613-1804688415-YfpcY.jpg"
+Picture.create user: provider, path: "http://vignette2.wikia.nocookie.net/ironman/images/9/9f/Captain-america-civil-war-iron-man-xlvi-sixth-scale-marvel-silo-902708.png/revision/latest?cb=20160610130645"

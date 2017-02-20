09783a0b11cd8d81cc6bfb48199ee741b089aca6
pierre.l <pierre.l@snapkin.fr>
2017-02-20 19:48:19 +0100

    Full redirection and welcome.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,9 @@
 class PagesController < ApplicationController
+  def index
+    if user_signed_in?
+      redirect_to '/pictures'
+    else
+      redirect_to '/pages/welcome'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'pages#index'
+
+  get '/pages/welcome', to: 'pages#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
